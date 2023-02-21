647ff1bbc0f46c388123d0df64b5d3cfc7ad062b
曾军杰 <lanfein9@hotmail.com>
2023-02-21 10:52:20 +0800

    feats: 增加登录页逻辑

--- src/views/login/components/LoginForm.tsx
+++ src/views/login/components/LoginForm.tsx
@@ -1,14 +1,27 @@
+import { Login } from "@/api/interface";
 import { CloseCircleOutlined, LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Form, Input, message } from "antd";
-import { FC } from "react";
+import md5 from "js-md5";
+import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { loginApi } from "../../../api/modules/login";
 
 const LoginForm: FC = () => {
+	const [loading, setLoading] = useState<boolean>(false);
+	const [form] = Form.useForm();
 	const navigate = useNavigate();
-	const onFinish = (value: any) => {
-		console.log(value);
-		message.success("登录成功");
-		navigate("/home");
+	const onFinish = async (loginForm: Login.ReqLoginForm) => {
+		try {
+			setLoading(true);
+			loginForm.password = md5(loginForm.password);
+			await loginApi(loginForm);
+			message.success("登录成功");
+			navigate("/home");
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setLoading(false);
+		}
 	};
 
 	const onFinishFailed = (errorInfo: any) => {
@@ -17,6 +30,7 @@ const LoginForm: FC = () => {
 
 	return (
 		<Form
+			form={form}
 			name="basic"
 			labelCol={{ span: 5 }}
 			initialValues={{ remember: true }}
@@ -29,11 +43,13 @@ const LoginForm: FC = () => {
 				<Input placeholder="用户名: admin / user" prefix={<UserOutlined />} />
 			</Form.Item>
 			<Form.Item name="password" rules={[{ required: true, message: "Please input your password!" }]}>
-				<Input placeholder="密码: 123456" prefix={<LockOutlined />} />
+				<Input.Password autoComplete="new-password" placeholder="密码: 123456" prefix={<LockOutlined />} />
 			</Form.Item>
 			<Form.Item className="login-btn">
-				<Button icon={<CloseCircleOutlined />}>重置</Button>
-				<Button type="primary" icon={<UserOutlined />}>
+				<Button onClick={() => form.resetFields()} icon={<CloseCircleOutlined />}>
+					重置
+				</Button>
+				<Button type="primary" htmlType="submit" loading={loading} icon={<UserOutlined />}>
 					登录
 				</Button>
 			</Form.Item>
